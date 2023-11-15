b409f3e750923167ab842753e760835078f67868
Alishan Anwar <78029712+alishan008@users.noreply.github.com>
2023-11-15 20:55:56 +1100

    Update Code.py

--- Code.py
+++ Code.py
@@ -4,7 +4,8 @@ WITH ranked_dates AS (
     f.b_date,
     s.s_date,
     s.price,
-    LEAD(s.s_date) OVER (PARTITION BY f.key ORDER BY s.s_date) AS next_s_date
+    FIRST_VALUE(s.s_date) OVER (PARTITION BY f.key, f.b_date 
+                               ORDER BY CASE WHEN s.s_date >= f.b_date THEN s.s_date END) AS next_s_date
   FROM
     first_table f
   LEFT JOIN
@@ -14,20 +15,7 @@ WITH ranked_dates AS (
 SELECT
   key,
   b_date,
-  s_date,
+  COALESCE(next_s_date, s_date) AS s_date,
   price
-FROM (
-  SELECT
-    key,
-    b_date,
-    s_date,
-    price,
-    ROW_NUMBER() OVER (PARTITION BY key, b_date ORDER BY 
-                       CASE WHEN b_date = s_date THEN 0 ELSE 1 END, s_date) AS row_num
-  FROM
-    ranked_dates
-  WHERE
-    b_date = s_date OR (b_date < s_date AND (next_s_date = b_date OR next_s_date IS NULL))
-) result
-WHERE
-  row_num = 1
+FROM
+  ranked_dates
