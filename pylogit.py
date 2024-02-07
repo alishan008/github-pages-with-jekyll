f15f360b2325ab8ee1e1a03be8621968bb7fe25c
Alishan Anwar <78029712+alishan008@users.noreply.github.com>
2024-02-07 14:13:02 +1100

    Update pylogit.py

--- pylogit.py
+++ pylogit.py
@@ -40,3 +40,18 @@ print(model.get_statsmodels_summary())
 # Access class probabilities and other model attributes
 class_probs = model.get_mixing_distribution()
 class_memberships = model.predict(data=df, mixing_id_col='id', choice_col='choice_column')
+---------------------------------------------------------------------------------------------
+
+model_specification = {
+    'covariate1': (covariates.index('grade'), 'grade', 'all_same'),
+    'covariate2': (covariates.index('cat'), 'cat', 'all_same'),
+    'covariate3': (covariates.index('beds'), 'beds', 'all_same'),
+    'covariate4': (covariates.index('bkg_wp'), 'bkg_wp', 'all_same'),
+    'covariate5': (covariates.index('fare'), 'fare', 'all_same'),
+    'covariate6': (covariates.index('size'), 'size', 'all_same'),
+    'covariate7': (covariates.index('location'), 'location', 'all_same'),
+    'covariate8': (covariates.index('low_cost'), 'low_cost', 'all_same'),
+    'covariate9': (covariates.index('past_customer'), 'past_customer', 'all_same'),
+    'covariate10': (covariates.index('duration_grp'), 'duration_grp', 'all_same'),
+    'covariate11': (covariates.index('premium_paid'), 'premium_paid', 'all_same'),
+}
